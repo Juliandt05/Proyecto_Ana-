6d3210db51c874dffa82d88b38191ce62877f301
Julio Chen <al_ychen@iessierradelasvillas.org>
2023-02-13 10:59:12 +0100

    Text field creados

--- proyecto.py
+++ proyecto.py
@@ -2,8 +2,17 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "Proyecto Inicio sesión"
-    nombre=ft.TextField(label="Nombre", hint_text="Introduzca su nombre",)
-    page.padding (50)
-  
-    page.add(nombre)
-ft.app(target=main)
+    page.window_maximizable = False
+    page.window_minimized = False
+    page.window_max_height = 1500
+    page.window_max_width = 1500
+    page.bgcolor = ft.colors.BLACK
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    nombre_textfield = ft.TextField(label="Nombre",width=300)
+    contrasena_textfield = ft.TextField(label="Contraseña",width=300)
+    imagen = ft.Image(src="Logo_empresa.jpg",width=250,height=250)
+    page.update()
+
+    page.add(imagen,nombre_textfield,contrasena_textfield)
+ft.app(target=main,assets_dir="Imagenes")
---
+++ pruebas.py
@@ -0,0 +1,2 @@
+ column = ft.Column(alignment=ft.MainAxisAlignment.CENTER,controls=[nombre_textfield,contrasena_textfield])
+     page.add(column,imagen)
